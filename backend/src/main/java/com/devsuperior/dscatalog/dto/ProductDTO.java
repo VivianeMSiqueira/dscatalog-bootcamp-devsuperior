8dc0237481528d6348d15a63bfa52fc4b3ab8499
VivianeMSiqueira <vickymsa@hotmail.com>
2021-06-28 00:16:14 -0300

    Basic beans validation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 
@@ -13,10 +18,17 @@ public class ProductDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Não aceita espaços em branco.")
+	@Size(min = 5, max = 60, message = "O nome deve ter o mínimo de 5 e máximo de 60 caracteres.")
 	private String name;
 	private String description;
+	
+	@Positive(message = "Valor deve ser positivo.")
 	private Double price;
 	private String imgUrl;
+	
+	@PastOrPresent(message = "A data não pode ser futura.")
 	private Instant date;
 	
 	private List<CategoryDTO> categories = new ArrayList<>();
